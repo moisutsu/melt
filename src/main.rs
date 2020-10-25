0e23dc60a77f20b8cd941640b97ff2ff5cbedf25
moisutsu <moisutsu@gmail.com>
2020-10-25 21:28:13 +0900

    Add implementation for CUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Clap;
+use melt::{decompress, Opts};
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    decompress(opts.input_file)?;
+    Ok(())
 }
