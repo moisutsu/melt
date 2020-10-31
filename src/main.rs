3d23aa84e954e6fd92e76825f1ab5515a17a656e
moisutsu <moisutsu@gmail.com>
2020-10-31 23:02:19 +0900

    Add error for existence of file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Clap;
 use melt::{decompress, Opts};
+use std::path::Path;
 
 fn main() -> Result<()> {
     let opts = Opts::parse();
+    if !Path::new(&opts.input_file).exists() {
+        return Err(anyhow!(format!(
+            "The file {} does not exist",
+            opts.input_file
+        )));
+    }
     decompress(&opts.input_file)?;
     Ok(())
 }
