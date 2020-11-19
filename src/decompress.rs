dd74a7959aef5b866752d1599dfbfc53a32eb41f
moisutsu <moisutsu@gmail.com>
2020-11-19 21:49:14 +0900

    Add function that decompress bz2 using bunzip2

--- src/decompress.rs
+++ src/decompress.rs
@@ -7,6 +7,7 @@ pub fn decompress(file_name: &str) -> Result<()> {
         Zip => decompress_zip(file_name),
         Gz => decompress_gz(file_name),
         Z => decompress_z(file_name),
+        Bz2 => decompress_bz2(file_name),
         Tar => decompress_tar(file_name),
         TarGz => decompress_tar_gz(file_name),
         TarBz2 => decompress_tar_bz2(file_name),
@@ -30,6 +31,11 @@ fn decompress_gz(file_name: &str) -> Result<()> {
 
 fn decompress_z(file_name: &str) -> Result<()> {
     spawn_command("uncompress", &[file_name])?;
+    Ok(())
+}
+
+fn decompress_bz2(file_name: &str) -> Result<()> {
+    spawn_command("bunzip2", &[file_name])?;
     Ok(())
 }
 
