f31f9a0bcaee00b3cf956c9ce90a7365db31a0b3
moisutsu <moisutsu@gmail.com>
2020-11-19 16:31:11 +0900

    Change get_extention arg type &String -> &str

--- tests/get_extention.rs
+++ tests/get_extention.rs
@@ -2,32 +2,23 @@ use melt::{get_extention, Ext};
 
 #[test]
 fn test_get_extention() {
-    assert_eq!(get_extention(&"sample.zip".to_string()), Some(Ext::Zip));
-    assert_eq!(get_extention(&"sample.tar".to_string()), Some(Ext::Tar));
+    assert_eq!(get_extention("sample.zip"), Some(Ext::Zip));
+    assert_eq!(get_extention("sample.tar"), Some(Ext::Tar));
+    assert_eq!(get_extention("sample.tar.gz"), Some(Ext::TarGz));
+    assert_eq!(get_extention("sample.tar.bz2"), Some(Ext::TarBz2));
+    assert_eq!(get_extention("sample.tar.xz"), Some(Ext::TarXz));
     assert_eq!(
-        get_extention(&"sample.tar.gz".to_string()),
-        Some(Ext::TarGz)
-    );
-    assert_eq!(
-        get_extention(&"sample.tar.bz2".to_string()),
-        Some(Ext::TarBz2)
-    );
-    assert_eq!(
-        get_extention(&"sample.tar.xz".to_string()),
-        Some(Ext::TarXz)
-    );
-    assert_eq!(
-        get_extention(&"sample.rs".to_string()),
+        get_extention("sample.rs"),
         Some(Ext::Other("rs".to_string()))
     );
     assert_eq!(
-        get_extention(&"sample.rs.gz".to_string()),
+        get_extention("sample.rs.gz"),
         Some(Ext::Other("rs.gz".to_string()))
     );
-    assert_eq!(get_extention(&"sample".to_string()), None);
-    assert_eq!(get_extention(&".sample".to_string()), None);
+    assert_eq!(get_extention("sample"), None);
+    assert_eq!(get_extention(".sample"), None);
     assert_eq!(
-        get_extention(&".sample.gz".to_string()),
+        get_extention(".sample.gz"),
         Some(Ext::Other("gz".to_string()))
     );
 }
