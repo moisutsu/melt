e13abae721e6bd8df5af7a410b8530ae56217614
moisutsu <moisutsu@gmail.com>
2021-07-28 17:13:31 +0900

    Add a description of the command line arguments

--- src/opts.rs
+++ src/opts.rs
@@ -3,5 +3,6 @@ use clap::{crate_authors, crate_description, crate_version, Clap};
 #[derive(Clap)]
 #[clap(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
 pub struct Opts {
+    #[clap(about = "Files to decompress")]
     pub files: Vec<String>,
 }
