ddb8bc9d2361e164ba6aaab87e43b7ec8761c582
moisutsu <moisutsu@gmail.com>
2021-11-04 12:55:36 +0900

    Bump clap version to 3.0.0-beta.5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use clap::Clap;
+use clap::Parser;
 use melt::{decompress, Opts};
 
 fn main() -> Result<()> {
--- src/opts.rs
+++ src/opts.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
-use clap::{crate_authors, crate_description, crate_version, Clap};
+use clap::{crate_authors, crate_description, crate_version, Parser};
 
-#[derive(Clap)]
+#[derive(Parser)]
 #[clap(version = crate_version!(), author = crate_authors!(), about = crate_description!())]
 pub struct Opts {
     #[clap(about = "Files to decompress", parse(from_os_str))]
